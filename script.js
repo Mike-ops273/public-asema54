387e2c04fc3ed8887fa78bd6307ac65a575127ba
Mike-ops273 <73826014+Mike-ops273@users.noreply.github.com>
2021-05-31 13:13:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('testing');
+//nav menu
+const menuButton = document.querySelector('.menu-button');
+let menuOpen = false; 
+menuButton.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuButton.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuButton.classList.remove('open'); 
+        menuOpen = false; 
+    }
+})
