b557b3d769bbbd2d18db30511004b28a37c48e0e
Mike-ops273 <73826014+Mike-ops273@users.noreply.github.com>
2021-06-02 10:28:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//nav menu
+//NAVIGATION MENU
 const menuButton = document.querySelector('.menu-button');
 let menuOpen = false; 
 menuButton.addEventListener('click', () => {
@@ -10,3 +10,20 @@ menuButton.addEventListener('click', () => {
         menuOpen = false; 
     }
 })
+
+//AUTOMATIC SLIDESHOW
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 3.5 seconds
+} 
+
+
